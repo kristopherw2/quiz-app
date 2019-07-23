30d8491ca85ed98c2b8dfdfdebb905710b452b47
Kristopher Williams <kristopher.williams2@gmail.com>
2019-07-22 20:14:22 -0500

    added styling

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function updateQuestionNumber() {
 }
 
 //update quest counter txt
-function updateQuestionCounter(){
-    $('.question-counter').text(`${questionNumber+1} of 6`);
+function updateQuestionCounter() {
+    $('.question-counter').text(`Question: ${questionNumber + 1}/6`);
 }
 
 //increases score
@@ -54,7 +54,7 @@ const QUESTIONS = [
         correctAnswer: 'Mawashi'
     },
 ];
-
+//for images
 const imageStore = [
     {
         image: 'https://i.imgur.com/HXvNcxf.jpg',
@@ -109,8 +109,8 @@ function startQuiz() {
         event.preventDefault();
         $('.start-view').remove();
         $('#question-view').css('display', 'block')
-        if(questionNumber < 6){
-        $('.question-counter').text(`${questionNumber+1} of 6`);
+        if (questionNumber < 6) {
+            $('.question-counter').text(`Question: ${questionNumber + 1}/6`);
         }
     });
 }
@@ -119,42 +119,44 @@ function startQuiz() {
 //generates the questions
 function generateQuestion() {
     if (questionNumber < QUESTIONS.length) {
-        return `<div class="questions-${questionNumber}">
-     <section>
-         <h2>${QUESTIONS[questionNumber].questionText}</h2>
-         <ul>
-             <form>
-                 <fieldset>
-                     <li>
-                         <label name="question-answers">
-                             <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[0]}" required>
-                             <span>${QUESTIONS[questionNumber].answers[0]}</span>
-                         </label>
-                     </li>
-                     <li>
-                         <label name="question-answers">
-                             <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[1]}" required>
-                             <span>${QUESTIONS[questionNumber].answers[1]}</span>
-                         </label>
-                     </li>
-                     <li>
-                     <label name="question-answers">
-                         <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[2]}" required>
-                         <span>${QUESTIONS[questionNumber].answers[2]}</span>
-                     </label>
-                 </li>
-                 <li>
-                 <label name="question-answers">
-                     <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[3]}" required>
-                     <span>${QUESTIONS[questionNumber].answers[3]}</span>
-                 </label>
-             </li>
-                     <input class="answer-submit" type="submit" value="Submit Your Answer">
-                 </fieldset>
-             </form>
-         </ul>
-     </section>
- </div>`
+        return `<section class="css-question-list">
+            <h2>Question: ${QUESTIONS[questionNumber].questionText}</h2>
+            <ul>
+                <form>
+                    <fieldset>
+                        <li>
+                            <label name="question-answers">
+                                <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[0]}"
+                                    required>
+                                <span>${QUESTIONS[questionNumber].answers[0]}</span>
+                            </label>
+                        </li>
+                        <li>
+                            <label name="question-answers">
+                                <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[1]}"
+                                    required>
+                                <span>${QUESTIONS[questionNumber].answers[1]}</span>
+                            </label>
+                        </li>
+                        <li>
+                            <label name="question-answers">
+                                <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[2]}"
+                                    required>
+                                <span>${QUESTIONS[questionNumber].answers[2]}</span>
+                            </label>
+                        </li>
+                        <li>
+                            <label name="question-answers">
+                                <input type="radio" name="choice" value="${QUESTIONS[questionNumber].answers[3]}"
+                                    required>
+                                <span>${QUESTIONS[questionNumber].answers[3]}</span>
+                            </label>
+                        </li>
+                        <input class="answer-submit css-button" type="submit" value="Submit Your Answer">
+                    </fieldset>
+                </form>
+            </ul>
+        </section>`
     } else {
         renderResults();
     }
@@ -181,22 +183,26 @@ function answerSelectedAndSubmitted() {
 
 //will return results if answered correct
 function questionAnsweredCorrect() {
-    $('#question-view').html(`<div class="feedback">
+    $('#question-view').html(`<section class="feedback">
     <p>You've got it right!</p>
-    <img class="image-answers" src="${imageStore[questionNumber].image}" alt="${imageStore[questionNumber].description}">
-    <button type="button" class="next-question">Next Question</button>
-</div>`)
+    <div class="image-answers">
+        <img class="image-answers" src="${imageStore[questionNumber].image}" alt="${imageStore[questionNumber].description}">
+    </div>
+    <button type="button" class="next-question css-button">Next Question</button>
+</section>`)
     addScore();
-$('.score-counter').text(`${score} of 6`);
+    $('.score-counter').text(`Score: ${score}`);
     //console.log('Question was answered correct')
 }
 //will return incorrect answer screen
 function questionAnsweredIncorrect() {
-    $('#question-view').html(`<div class="feedback">
+    $('#question-view').html(`<section class="feedback">    
     <p>You answered that incorrectly the correct answer would be ${QUESTIONS[questionNumber].correctAnswer}</p>
-    <img class="image-answers" src="${imageStore[questionNumber].image}" alt="${imageStore[questionNumber].description}">
-    <button type="button" class="next-question">Next Question</button>
-</div>`)
+    <div class="image-answers">
+        <img class="image-answers" src="${imageStore[questionNumber].image}" alt="${imageStore[questionNumber].description}">
+    </div>
+    <button type="button" class="next-question css-button">Next Question</button>
+</section>`)
 
     //console.log('Answer is incorrect!')
 }
@@ -207,7 +213,7 @@ function renderNextQuestion() {
         updateQuestionNumber();
         renderQuestion();
         answerSelectedAndSubmitted();
-        if(questionNumber < 6){
+        if (questionNumber < 6) {
             updateQuestionCounter();
         }
         //consoleQuestionsArray();
@@ -218,15 +224,21 @@ function renderNextQuestion() {
 //this function should render the results of the entire quiz
 function renderResults() {
     if (score < 6) {
-        $('#question-view').html(`<p>The End! Your score was ${score} of 6. You can do better!</p><button class="start-over">START OVER</button>`)
+        $('#question-view').html(`<section class="feedback">
+        <p>The End! Your score was ${score} of 6. You can do better!</p>
+        <button class="start-over css-button">START OVER</button>
+        </section>`)
     } else {
-        $('#question-view').html(`<p>You did perfect! You're worthy of the rank Yokozuna!<button class="start-over">START OVER</button>`)
+        $('#question-view').html(`<section class="feedback">
+        <p>You did perfect! You're worthy of the rank Yokozuna!</p>
+        <button class="start-over css-button">START OVER</button>
+    </section>`)
     }
 }
 
 //function to start quiz over
-function startOver(){
-    $('#question-view').on('click', '.start-over', function(){
+function startOver() {
+    $('#question-view').on('click', '.start-over', function () {
         location.reload();
     });
 }
